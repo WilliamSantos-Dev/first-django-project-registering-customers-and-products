e688c958e5d32bf9682fe7e7dd0d312627d59fca
william <williambatista958@gmail.com>
2023-07-16 12:16:05 -0300

    Primeiro projeto Django

---
+++ core/static/js/script.js
@@ -0,0 +1,3 @@
+function teste(){
+    alert('Esse aquivo é um teste e esta em JS')
+}
---
+++ core/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.template import loader
+from .models import Produto
+def index(request):
+    produtos = Produto.objects.all()
+    if str(request.user) == 'AnonymousUser':
+        statusLogin = 'Usuário não logado'
+    else:
+        statusLogin = 'Usuário logado'
+    context = {
+        'curso': 'Programação Web com Django Framework',
+        'logado': statusLogin,
+        'produtos': produtos,
+    }
+    return render(request, 'index.html', context)
+
+def contato(request):
+    return render(request, 'contato.html')
+
+def produto(request, pk):
+    #prod = Produto.objects.get(id=pk)
+    prod = get_object_or_404(Produto, id=pk)
+    context = {
+        'produto': prod
+    }
+    return render(request, 'produto.html', context)
+
+def error404(request, exception):
+    template = loader.get_template('error404.html')
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf-8', status=404)
+
+def error500(request):
+    template = loader.get_template('error500.html')
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf-8', status=500)
